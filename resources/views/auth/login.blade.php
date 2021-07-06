66b5453785ca3b3030246f16656e16b4203c6644
Silas Silva <silvasilas66@gmail.com>
2021-07-05 22:05:56 -0300

    Remaking the login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,57 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+    <!-- Tailwind -->
+    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <section class="flex flex-col md:flex-row h-screen items-center">
 
-        <x-jet-validation-errors class="mb-4" />
+        <div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+          <img src="https://blog.leucotron.com.br/wp-content/uploads/2017/12/desmistificando-o-pabx-entenda-os-beneficios-e-aplicacoes.jpeg" alt="" class="w-full h-full object-cover">
+        </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+        <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+            <div class="w-full h-100">
+                <div class="p-6">
+                    <a href="index.html" class="flex items-center justify-center text-blue-700 text-3xl font-semibold uppercase hover:text-gray-300">VoiceApp</a>
+                </div>
+
+                <form class="mt-6" action="{{ route('login') }}" method="POST">
+                    @csrf
+
+                    <div>
+                        <label class="block text-gray-700">E-mail</label>
+                        <input type="email" name="email" :value="old('email')" placeholder="Insira seu e-mail" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required>
+                    </div>
+
+                    <div class="mt-4">
+                        <label class="block text-gray-700">Senha</label>
+                        <input type="password"name="password" autocomplete="current-password" placeholder="Insira sua senha" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required>
+                    </div>
+
+                    <x-jet-validation-errors class="mb-4" />
+
+                    @if (session('status'))
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="block mt-4">
+                        <label for="remember_me" class="flex items-center">
+                            <x-jet-checkbox id="remember_me" name="remember" />
+                            <span class="ml-2 text-sm text-gray-600">Lembre-me</span>
+                        </label>
+                    </div>
+
+                    <button type="submit" class="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">
+                        Entrar
+                    </button>
+                </form>
+
+                <hr class="my-6 border-gray-300 w-full">
+
+                <p class="mt-8">Precisa de uma conta? <a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">Contate o suporte</a></p>
+
             </div>
-        @endif
+        </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+    </section>
 </x-guest-layout>
